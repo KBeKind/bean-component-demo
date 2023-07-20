bd8b6b31ed6847de0d1e3ec406bcc87aedec65ba
KBeKind <79383539+KBeKind@users.noreply.github.com>
2023-07-20 07:30:18 -0500

    initial commit

---
+++ src/main/java/dev/kbekind/beancomponentdemo/BeanComponentDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.kbekind.beancomponentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanComponentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanComponentDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kbekind/beancomponentdemo/BeanComponentDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kbekind.beancomponentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanComponentDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
